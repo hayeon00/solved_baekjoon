1a9a9a5185810db0bc938467a3be206defc41636
hayeon00 <103185805+hayeon00@users.noreply.github.com>
2025-03-15 21:32:06 +0900

    Update update.py

--- update.py
+++ update.py
@@ -3,52 +3,53 @@
 import os
 from urllib import parse
 
-HEADER="""# 
-# generate_readme_automatically
-
----
+HEADER="""#
+# 백준, 프로그래머스 문제 풀이 목록
 """
 
 def main():
-    content = ""
-    content += HEADER
+  content = ""
+  content += HEADER
+  
+  directories = []
+  solveds = []
+  
+  for root, dirs, files in os.walk("."):
+    dirs.sort()
+    if root == '.':
+      for dir in ('.git', '.github'):
+        try:
+          dirs.remove(dir)
+        except ValueError:
+          pass
+      continue
     
-    directories = [];
-
-    for root, dirs, files in os.walk("."):
-        dirs.sort()
-        if root == '.':
-            for dir in ('.git', '.github'):
-                try:
-                    dirs.remove(dir)
-                except ValueError:
-                    pass
-            continue
-
-        category = os.path.basename(root)
+    category = os.path.basename(root)
+    
+    if category == 'images':
+      continue
+      
+    directory = os.path.basename(os.path.dirname(root))
+    
+    if directory == '.':
+      continue
+      
+    if directory not in directories:
+      if directory in ["백준", "프로그래머스"]:
+        content += "## 📚 {}\n".format(directory)
+      else:
+        content += "### 🚀 {}\n".format(directory)
+        content += "| 문제번호 | 링크 |\n"
+        content += "| ----- | ----- |\n"
+      directories.append(directory)
+      
+    for file in files:
+      if category not in solveds:
+        content += "|{}|[링크]({})|\n".format(category, parse.quote(os.path.join(root, file)))
+        solveds.append(category)
         
-        if category == 'images':
-            continue
-        
-        directory = os.path.basename(os.path.dirname(root))
-        
-        if directory == '.':
-            if len(files) == 1:
-                content += "### [{}]({})\n".format(category, parse.quote(os.path.join(root, files[0])))
-                directories.append(category)
-            continue
-            
-        if directory not in directories:
-            content += "### {}\n".format(directory)
-            directories.append(directory)
-
-        for file in files:
-            content += "- [{}]({})\n".format(category, parse.quote(os.path.join(root, file)))
-        content += "\n"
-
-    with open("README.md", "w") as fd:
-        fd.write(content)
-
-
+  with open("README.md", "w") as fd:
+    fd.write(content)
+    
 if __name__ == "__main__":
-    main()
+  main()
